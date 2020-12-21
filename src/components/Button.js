78f6837ca9bdaf4600bda09d36f404321e783204
momchilmitev <momchilmitev87@gmail.com>
2020-12-21 19:09:54 +0100

    added the logic implementation with the contet api

---
+++ src/components/Button.js
@@ -0,0 +1,18 @@
+import React from "react";
+import LanguageContext from "../contexts/LanguageContext";
+
+class Button extends React.Component {
+  renderSubmit(value) {
+    return value === "english" ? "Submit" : "Voorleggen";
+  }
+
+  render() {
+    return (
+      <button className="ui button primary">
+        <LanguageContext>{(value) => this.renderSubmit(value)}</LanguageContext>
+      </button>
+    );
+  }
+}
+
+export default Button;
